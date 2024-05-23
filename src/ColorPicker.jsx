739536ec830da7ee52bfc17cfc4d8a02e7eaf946
Ngoc <ngoc.held9@gmail.com>
2024-05-23 15:14:11 +0900

    component completed

---
+++ src/ColorPicker.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+
+function ColorPicker (){
+
+  const [color,setColor] = useState('#fff');
+
+
+  const handelColorChange = (event) => {
+    setColor(event.target.value);
+  }
+
+  return(<div className="color-picker-container">
+          <h1>Color Picker</h1>
+          <div className="color-display" style={{backgroundColor: color}}>
+            <p>Picked Color: {color}</p>
+          </div>
+          <lable>Select a color</lable>
+          <input type="color" onChange={handelColorChange}></input>
+        </div>);
+}
+export default ColorPicker
